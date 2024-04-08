57f5fa2f38c283b25ee09508ad9c5b02a1820f29
Bogdan Kozak <bkozak04@gmail.com>
2024-04-08 15:18:46 +0300

    Fivth step

---
+++ spaceship/routers/matrix.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+import numpy as np
+
+router = APIRouter()
+
+@router.get("")
+async def multiply_matrices():
+    matrix_a = np.random.randint(1, 11, size=(10, 10)).tolist()
+    matrix_b = np.random.randint(1, 11, size=(10, 10)).tolist()
+
+    product = np.dot(matrix_a, matrix_b).tolist()
+
+    return {"matrix_a": matrix_a, "matrix_b": matrix_b, "product": product}
